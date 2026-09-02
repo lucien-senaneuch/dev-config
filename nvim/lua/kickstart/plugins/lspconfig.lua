455a81d9eaeb59a82f8f7c43b792f5d9bd2e9f80
Lucien Senaneuch <lucien.senaneuch@ilpunto-jp.com>
2026-09-02 16:58:35 +0900

    feat: adds lsp to nvim

--- nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/lua/kickstart/plugins/lspconfig.lua
@@ -113,7 +113,9 @@ local servers = {
   --
   -- But for many setups, the LSP (`ts_ls`) will work just fine
   -- ts_ls = {},
-
+  roslyn_ls = {},
+  vtsls = {},
+  angularls = {},
   stylua = {}, -- Used to format Lua code
 
   -- Special Lua Config, as recommended by neovim help docs
